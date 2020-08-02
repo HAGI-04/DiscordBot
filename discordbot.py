6700cbb1f89140dfcb9871339ef9a83a2c4b570e
HAGI-04 <42399040+HAGI-04@users.noreply.github.com>
2020-08-02 22:20:30 +0900

    test

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,52 @@
-from discord.ext import commands
+import discord
 import os
-import traceback
+import datetime
 
-bot = commands.Bot(command_prefix='/')
+
+BOT_CHANNEL_ID = 599530740148994050 #ゆずぽん第一支部のVC通知用テキストチャンネルのID
+
 token = os.environ['DISCORD_BOT_TOKEN']
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+client = discord.Client()
+
+bot_channel = None
+last_update = ""
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_ready():
+    global bot_channel
+    global last_update
+    
+    bot_channel = client.get_channel(BOT_CHANNEL_ID)
+
+    dt_now = datetime.datetime.now(JST)
+    last_update = dt_now.strftime("[%m/%d %H:%M:%S]")
+
+@client.event   # VC入退室通知
+async def on_voice_state_update(member, before, after):
+    mem_name = member.display_name
+    bef_cha = before.channel
+    aft_cha = after.channel
+    dt_now = datetime.datetime.now(JST)
+
+    if bef_cha is not aft_cha:  # 入退室および移動のみ検知、ミュートなどの状態変更は除く
+        if bef_cha is None:     # 入室
+            m = ":blue_circle: IN : [**" + mem_name + "**] "
+        elif aft_cha is None:   # 退室
+            m = ":red_circle: OUT : [**" + mem_name + "**] "
+        m += dt_now.strftime("||[%m/%d %H:%M:%S]||")  # 現在時刻付与
+    
+    if member.guild is bot_channel.guild:
+        await bot_channel.send(m)
+
+@client.event   # メッセージ投稿検知イベント
+async def on_message(message):
+    if client.user != message.author:
+        if message.content == "/info":
+            m = "ボット発言チャンネル：" + str(bot_channel) + "\n"
+            m += "最終アップデート " + last_update + "\n"
+            await message.channel.send(m)
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
-
-bot.run(token)
+client.run(token)

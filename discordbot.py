351e55b1bba43a6cbfb5973249e1dc80f1ccbfb5
HAGI-04 <42399040+HAGI-04@users.noreply.github.com>
2020-08-03 17:27:09 +0900

    複数サーバー対応

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,12 @@ import os
 import datetime
 
 
-BOT_CHANNEL_ID = 599530740148994050 #ゆずぽん第一支部のVC通知用テキストチャンネルのID
+guildID2channelID = {
+    597316369989632002:739749926057017356,  # botテスト鯖
+    589468743214563343:599530740148994050,  # ゆずぽん
+    698397334274310174:739760013568114708   # MIKUMARI
+    }
+
 
 token = os.environ['DISCORD_BOT_TOKEN']
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
@@ -15,13 +20,11 @@ last_update = ""
 
 @client.event
 async def on_ready():
-    global bot_channel
     global last_update
-    
-    bot_channel = client.get_channel(BOT_CHANNEL_ID)
 
     dt_now = datetime.datetime.now(JST)
     last_update = dt_now.strftime("[%m/%d %H:%M:%S]")
+
 
 @client.event   # VC入退室通知
 async def on_voice_state_update(member, before, after):
@@ -37,15 +40,25 @@ async def on_voice_state_update(member, before, after):
             m = ":red_circle: OUT : [**" + mem_name + "**] "
         m += dt_now.strftime("||[%m/%d %H:%M:%S]||")  # 現在時刻付与
     
-    if member.guild is bot_channel.guild:
-        await bot_channel.send(m)
+    # 通知するサーバーを特定
+    channelID = guildID2channelID[member.guild.id]
+    bot_channel = client.get_channel(channelID)
+
+    await bot_channel.send(m)
+
 
 @client.event   # メッセージ投稿検知イベント
 async def on_message(message):
     if client.user != message.author:
         if message.content == "/info":
+
+            # 情報を表示するサーバーを特定
+            channelID = guildID2channelID[message.guild.id]
+            bot_channel = client.get_channel(channelID)
+
             m = "ボット発言チャンネル：" + str(bot_channel) + "\n"
             m += "最終アップデート " + last_update + "\n"
+
             await message.channel.send(m)
 
 

499eb54f441b2f369e15ec02e0e0771297411307
HAGI-04 <42399040+HAGI-04@users.noreply.github.com>
2023-03-23 03:37:51 +0900

    もとにもどした

--- discordbot.py
+++ discordbot.py
@@ -1,102 +1,88 @@
 import discord
 import os
 import datetime
-import re
-from collections import defaultdict
 
 
-guild_id_to_channel_id = {
+guildID2channelID = {
     597316369989632002:739749926057017356,  # botテスト鯖
     589468743214563343:599530740148994050,  # ゆずぽん
     698397334274310174:739760013568114708   # MIKUMARI
-}
+    }
+
 
 token = os.environ['DISCORD_BOT_TOKEN']
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+bot_channel = None
 last_update = ""
-
-
-def get_current_timestamp():
-    dt_now = datetime.datetime.now(JST)
-    return dt_now.strftime("[%m/%d %H:%M:%S]")
 
 
 @client.event
 async def on_ready():
     global last_update
 
-    last_update = get_current_timestamp()
-    print(f"Bot is ready. Last update: {last_update}")
-
-
-async def send_voice_state_update_message(member, message):
-    channel_id = guild_id_to_channel_id[member.guild.id]
-    bot_channel = client.get_channel(channel_id)
-    await bot_channel.send(message)
-
-
-async def read_voice_history(guild_id):
-    channel_id = guild_id_to_channel_id[guild_id]
-    bot_channel = client.get_channel(channel_id)
-
-    now = datetime.datetime.now(JST)
-    seven_days_ago = now - datetime.timedelta(days=7)
-
-    voice_history = defaultdict(list)
-    async for message in bot_channel.history(after=seven_days_ago, oldest_first=True):
-        if message.author == client.user:
-            match = re.match(r"(?::blue_circle: IN|:red_circle: OUT) : \[\*\*(.+?)\*\*\] \|\|(\[\d{2}/\d{2} \d{2}:\d{2}:\d{2}\])\|\|", message.content)
-            if match:
-                action, member_name, timestamp = match.group(1), match.group(2), match.group(3)
-                voice_history[member_name].append((action, timestamp))
-    return voice_history
-
-
-def generate_gantt_chart(voice_history):
-    gantt_chart = "```\n"
-    for member_name, actions in voice_history.items():
-        gantt_chart += f"{member_name}:\n"
-        for action, timestamp in actions:
-            gantt_chart += f"{action} {timestamp}\n"
-        gantt_chart += "\n"
-    gantt_chart += "```"
-    return gantt_chart
+    dt_now = datetime.datetime.now(JST)
+    last_update = dt_now.strftime("[%m/%d %H:%M:%S]")
 
 
 @client.event   # VC入退室通知
 async def on_voice_state_update(member, before, after):
-    member_name = member.display_name
-    before_channel = before.channel
-    after_channel = after.channel
-    timestamp = get_current_timestamp()
+    mem_name = member.display_name
+    bef_cha = before.channel
+    aft_cha = after.channel
+    dt_now = datetime.datetime.now(JST)
 
-    if before_channel != after_channel:  # 入退室および移動のみ検知、ミュートなどの状態変更は除く
-        if before_channel is None:  # 入室
-            message = f":blue_circle: IN : [**{member_name}**] ||{timestamp}||"
-        elif after_channel is None:  # 退室
-            message = f":red_circle: OUT : [**{member_name}**] ||{timestamp}||"
-        await send_voice_state_update_message(member, message)
+    if bef_cha is not aft_cha:  # 入退室および移動のみ検知、ミュートなどの状態変更は除く
+        if bef_cha is None:     # 入室
+            m = ":blue_circle: IN : [**" + mem_name + "**] "
+        elif aft_cha is None:   # 退室
+            m = ":red_circle: OUT : [**" + mem_name + "**] "
+        m += dt_now.strftime("||[%m/%d %H:%M:%S]||")  # 現在時刻付与
+    
+    # 通知するサーバーを特定
+    channelID = guildID2channelID[member.guild.id]
+    bot_channel = client.get_channel(channelID)
+
+    await bot_channel.send(m)
 
 
 @client.event   # メッセージ投稿検知イベント
 async def on_message(message):
+
     # ボット自身の発言を検知しないよう判別
     if client.user != message.author:
-        if message.content.startswith("/info"):
-            channel_id = guild_id_to_channel_id[message.guild.id]
-            bot_channel = client.get_channel(channel_id)
-            info_message = f"ボット発言チャンネル：{bot_channel}\n最終アップデート {last_update}\n"
-            await message.channel.send(info_message)
-        elif message.content.startswith("/gantt"):
-            await message.channel.send("Reading voice history...")
-            voice_history = await read_voice_history(message.guild.id)
-            await message.channel.send("Generating gantt chart...")
-            gantt_chart = generate_gantt_chart(voice_history)
-            await message.channel.send("Sending gantt chart...")
-            await message.channel.send(gantt_chart)
 
+        # 情報を表示するサーバーを特定
+        channelID = guildID2channelID[message.guild.id]
+        bot_channel = client.get_channel(channelID)
+
+        if message.content == "/info":
+            m = "ボット発言チャンネル：" + str(bot_channel) + "\n"
+            m += "最終アップデート " + last_update + "\n"
+            await message.channel.send(m)
+
+        if message.content == '/plot':
+            await message.channel.send("processing...")
+            m = ""
+
+            # チャンネル内のVC入退室ログを取得
+            async for message in bot_channel.history(limit=200):
+                # botの発言のみ取得
+                if message.author == client.user:
+                    # 入退室通知文のみ取得
+                    msg = message.content
+
+                    if msg.startswith(":blue_circle: IN :") or msg.startswith(":red_circle: OUT :"):
+                        if msg.startswith(":b"):
+                            IO_state = "I"
+                        else:
+                            IO_state = "O"
+                        timedata = msg[-17:-3]
+                        author = msg[22:-24]
+                        m += IO_state + " : " + author + " : " + timedata + "/n"
+            
+            await message.channel.send(m)
 
 client.run(token)

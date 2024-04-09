77c0f7785672dd2e546dfaf47a44fc24ef08e0bf
HAGI <jwy720421@yahoo.co.jp>
2024-04-10 02:09:08 +0900

    ボット発言チャンネルを任意に設定できるようにした

--- discordbot.py
+++ discordbot.py
@@ -19,20 +19,25 @@ client = discord.Client(intents=intents)
 
 # 日本標準時
 JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+guild_id_2_channel_id = {}
 restarted_at = ""
 
-def fetch_channel_id(guild_id):
-    """サーバーIDに基づいてチャンネルIDを取得する"""
-    response = supabase.table('GuildID2ChannelID').select("channel_id").eq('guild_id', guild_id).execute()
-    if response.data:
-        return response.data[0]['channel_id']
-    return None
+def fetch_DB():
+    response = supabase.table('GuildID2ChannelID').execute()
+    return {d['guild_id']: d['channel_id'] for d in response.data} if response.data else {}
+
+def update_DB(guild_id, channel_id, guild_name):
+    response = supabase.table('GuildID2ChannelID').upsert({'guild_id': guild_id, 'channel_id': channel_id, 'guild_name': guild_name}).execute()
+    return response.status_code == 201
 
 @client.event
 async def on_ready():
     """ボットが起動したときの処理"""
     global restarted_at
     restarted_at = datetime.datetime.now(JST).strftime("[%m/%d %H:%M:%S]")
+
+    global guild_id_2_channel_id
+    guild_id_2_channel_id = fetch_DB()
 
 @client.event   
 async def on_voice_state_update(member, before, after):
@@ -44,7 +49,7 @@ async def on_voice_state_update(member, before, after):
     action = ":blue_circle: IN : " if before.channel is None else ":red_circle: OUT : "
     message = f"{action}[**{member.display_name}**] {time}"
     
-    channel_id = fetch_channel_id(member.guild.id)
+    channel_id = guild_id_2_channel_id.get(member.guild.id)
     if channel_id:
         await client.get_channel(channel_id).send(message)
 
@@ -54,15 +59,29 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    channel_id = fetch_channel_id(message.guild.id)
+    channel_id = guild_id_2_channel_id.get(message.guild.id)
     if channel_id is None:
         return
     
     bot_channel = client.get_channel(channel_id)
-    
+
     if message.content == "/info":
-        info_message = f"ボット発言チャンネル：{bot_channel.name}\n最終アップデート {restarted_at}\n"
+        info_message = f":white_check_mark: ボット発言チャンネル : {bot_channel.name}\n:white_check_mark: 最終アップデート : {restarted_at}\n"
         await message.channel.send(info_message)
+
+    elif message.content == "/set":
+        if update_DB(message.guild.id, message.channel.id, message.guild.name):
+            guild_id_2_channel_id = fetch_DB()
+            await message.channel.send(":white_check_mark: ボット投稿チャンネルをこのチャンネルにセットしました")
+        else:
+            await message.channel.send(":x: ボット投稿チャンネルの設定に失敗しました")
+    
+    elif message.content == "/list":
+        list_message = ":white_check_mark: 登録済みサーバー : ボット発言チャンネル\n```\n"
+        for guild_id, channel_id in guild_id_2_channel_id.items():
+            list_message += f"{client.get_guild(guild_id).name} : {client.get_channel(channel_id).name}\n"
+        list_message += "```"
+        await message.channel.send(list_message)
 
 client.run(DISCORD_BOT_TOKEN)
 
